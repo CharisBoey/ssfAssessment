3ec4f4670dc09397307026c9f773ef9df658022f
CharisBoey <charis_bsy@outlook.com>
2023-12-15 09:58:47 +0800

    Task1 complete

--- src/main/java/vttp/ssf/assessment/eventmanagement/EventmanagementApplication.java
+++ src/main/java/vttp/ssf/assessment/eventmanagement/EventmanagementApplication.java
@@ -1,15 +1,50 @@
 package vttp.ssf.assessment.eventmanagement;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import vttp.ssf.assessment.eventmanagement.models.Event;
+import vttp.ssf.assessment.eventmanagement.services.DatabaseService;
+
 @SpringBootApplication
-public class EventmanagementApplication {
+public class EventmanagementApplication implements CommandLineRunner{
+
+	@Autowired
+	DatabaseService databaseSvc;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventmanagementApplication.class, args);
 	}
 	
 	// TODO: Task 1
+	@Override
+	public void run(String... args) throws Exception{
+		String pathFileName = "C:\\Users\\chari\\Desktop\\ssfAssessment\\";
+		String fileName = "events.json";
+		pathFileName = pathFileName+fileName;
+		
+		/* TO CHECK THAT DATA HAS BEEN PASSED!!!
+		List<Event> checkList = databaseSvc.readFile(pathFileName);
+		
+		for(Event event: checkList){
+			System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!()");
+			System.out.println(event.getEventId());
+			System.out.println(event.getEventName());
+			System.out.println(event.getEventSize());
+			System.out.println(event.getEventDate());
+			System.out.println(event.getParticipants());
+			
+		} */
+
+	}
 
 }
--- src/main/java/vttp/ssf/assessment/eventmanagement/services/DatabaseService.java
+++ src/main/java/vttp/ssf/assessment/eventmanagement/services/DatabaseService.java
@@ -1,6 +1,58 @@
 package vttp.ssf.assessment.eventmanagement.services;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
+import vttp.ssf.assessment.eventmanagement.models.Event;
+
+@Service
 public class DatabaseService {
     
-    // TODO: Task 1
+    // TODO: Task 1 
+    public List<Event> readFile(String fileName) throws Exception{
+    
+        File file = new File(fileName);
+        InputStream is = new FileInputStream(file);
+        StringBuilder resultsfromFile = new StringBuilder();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(is))){
+            String line = "";
+            while((line = br.readLine())!=null){
+                resultsfromFile.append(line);
+            }
+        }
+
+        String data = resultsfromFile.toString();
+        System.out.println(data);
+
+        List<Event> eventListFromFile = new LinkedList<>();
+
+        JsonReader jsonReader = Json.createReader(new StringReader(data));
+        JsonArray eventArray = jsonReader.readArray();
+        for(int i=0; i<eventArray.size(); i++){
+            JsonObject indivEventContents = eventArray.get(i).asJsonObject();
+
+            Event retrievedEvent = new Event();
+            retrievedEvent.setEventId(indivEventContents.getInt("eventId"));
+            retrievedEvent.setEventName(indivEventContents.getString("eventName"));
+            retrievedEvent.setEventSize(indivEventContents.getInt("eventSize"));
+            retrievedEvent.setEventDate(indivEventContents.getInt("eventDate"));
+            retrievedEvent.setParticipants(indivEventContents.getInt("participants"));
+
+            eventListFromFile.add(retrievedEvent);
+        }
+        return eventListFromFile;
+    }
 }
